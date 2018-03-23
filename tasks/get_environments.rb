53d8c00f2a9e36290790644b4d6826ebbb78dbd9
jpadams <jeremy@puppetlabs.com>
2018-03-23 13:56:46 -0700

    Parens

--- tasks/get_environments.rb
+++ tasks/get_environments.rb
@@ -1,4 +1,4 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
-enviros = %x{/bin/ls -1 /etc/puppetlabs/code/environments/}.split("\n")
+enviros = %x(/bin/ls -1 /etc/puppetlabs/code/environments/).split("\n")
 puts enviros
